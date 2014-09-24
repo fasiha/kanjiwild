ca3b31d307fd2710a38239dde8e1d6469a9528f0
Ahmed Fasih <wuzzyview@gmail.com>
2014-09-23 23:36:28 -0400

    Better docs, backend, CSS (Pure), with statistics
    Squashed commit of the following:
    
    commit ac4880d885bdb4ee731c6a6e900d02f69f780bea
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Tweaking button
    
    commit d869c4c3b44841b50ca56424cbd8b50390a41396
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Tweaking stats
    
    commit 7c6a38adea2377650cff04136a5715adc36897a7
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        More CSS tweaks
    
    commit f90ec7602aa6bfe5fefdee965bf462413ce1220c
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Merge stats and answer key in HTML
    
    commit 47b967f8c51ce36c2f0da78bf250fb26ba4ba4b2
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Reordered stats
    
    commit b17319f5b6c75bebd04e780619e410e026058103
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Fiddling with colors
    
    commit 87bc0d87348f4869f6aa38459ba336ecea7a2f2a
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Bugfix: typing in the unchecked input now good
    
        Before we were setting the radio button when the unchecked input was
        clicked but that didn't fire the event.
    
    commit dba5d0d9260344f03fe26c3c1080934bfe24a6b8
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Skinny CSS
    
    commit 6c8b0102734529ff0f470614a38a398123ae80f1
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Stats added
    
    commit 99f1765b61de3ca10f35ca591a5544f083a6de93
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Had to use !important to override .pure-g font :(
    
    commit 33b90277af337041ad6636e00126b39849c49f99
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        generateKnown wouldn't fire when radios clicked
    
    commit 58d71f4bee2e80d6fd74b2cd2abcab4b45b14a6d
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Bugfix: reducing known kanji to remove highlights
    
    commit a4b52f217d2ff0f7832445184c65552ee0815965
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        More pure!
    
    commit 83688a98b8c2ab844e78d6ffafba4f48954e1bf4
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Input known kanji can be empty, no problem
    
    commit 96c98bb1bfea207b4e054ec99475ef5fc58551b8
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Pure!
    
    commit 2e1c825fc2609bb608857faa7fd8e0e4265f18de
    Author: Ahmed Fasih <wuzzyview@gmail.com>
    
        Documentation and code cleanup. Case-insensitive.
    
        Lots of cleanup!
    
        One enhancement: case-insentive keyword match.

--- kanjs.js
+++ kanjs.js
@@ -1,5 +1,26 @@
-// Contains shared global state
-var app = {"recognized-kanji" : {}};
+// Contains shared global state:
+//
+// - known-kanji: all kanji that the user wants to search for, based on Heisig
+//     number or textual entry (a dictionary, with keys as kanji)
+// - recognizable-kanji: subset of known-kanji that appears in the text the user
+//     has currently input (an array)
+// - unknown-kanji: the complementary set of input kanji that isn't in
+//     recognizable-kanji. An array.
+// - recognized-kanji: kanji that the user has previously indicated they
+//     recognize. It needn't be a subset of recognizable-kanji or even
+//     known-kanji
+//     since both the input text as well as the Heisig number/etc. could have
+//     changed. (A dictionary with keys as kanji.)
+// We want to keep track of which kanji the user has previously marked as
+// recognized even if it doesn't appear in the input text or in their list of
+// "known" kanji so that they don't have to keep selecting it as any of the app
+// inputs changes.
+var app = {
+    "known-kanji" : {},
+    "recognizable-kanji" : [],
+    "unknown-kanji" : [],
+    "recognized-kanji" : {},
+};
 
 // Kanji regexp
 var han = XRegExp('(\\p{Han})', 'g');
@@ -13,8 +34,9 @@ for (var i = 0; i < kanji.length; i++) {
     kanji2number[kanji.charAt(i)] = i;
 }
 
-// heisigNum >= 1
-var buildKnownKanjiDictHeisig = function(heisigNum) {
+// Given a maximum Heisig number in RTK 1+3, build a dictionary whose keys are
+// the kanji, with don't-care values. heisigNum should be a positive integer.
+function buildKnownKanjiDictHeisig(heisigNum) {
     var dict = {};
     heisigNum = Math.min(heisigNum, kanji.length);
     for (var i = 0; i < heisigNum; i++) {
@@ -23,47 +45,82 @@ var buildKnownKanjiDictHeisig = function(heisigNum) {
     return dict;
 };
 
-// knownKanji is a string
-var buildKnownKanjiDictInput =
-    function(knownKanjiString) {
-        var dict = {};
-        XRegExp.forEach(knownKanjiString, han,
-                        function(match, idx) { dict[match[0]] = idx; });
-        // var uniqueKanji = _.unique(knownKanjiString.match(han));
-        return dict;
-    }
+// Given a string containing kanji the user wishes to recognize, build a
+// dictionary whose keys are kanji in the string, with arbitrary values.
+// knownKanji is a string.
+function buildKnownKanjiDictInput(knownKanjiString) {
+    var dict = {};
+    XRegExp.forEach(knownKanjiString, han,
+                    function(match, idx) { dict[match[0]] = idx; });
+    // var uniqueKanji = _.unique(knownKanjiString.match(han));
+    return dict;
+}
 
+// Function that gets run whenever the recognition text has changed (or whenever
+// the set of known kanji changes).
+//
+// Needs app['known-kanji'] to exist, so build a set of known kanji first (based
+// on Heisig number or text input).
+//
+// This will take the input text, convert it to HTML (minor newline processing),
+// and annotate each kanji with DOM classes indicating whether it's a kanji and
+// if it ought to be recognizable.
+//
+// Also, very importantly, builds app['recognizable-kanji'], an array containing
+// the intersection of the kanji in the input text and
+// app['recognizable-kanji'], sorted in the same order as the kanji first appear
+// in the input text. This can be an array because, while we use its sort order
+// a couple of times (answer key, highlight all known), we don't ever have to
+// look up kanji in it, because these are tied to the DOM via D3.
 var japaneseInputChanged = function() {
-    if (app['known-kanji'] === undefined) {
-        console.error("Warning: can't parse input without set of known kanji.");
-        return;
-    }
     var text = d3.select('#input-japanese').property('value');
 
     // Do some basic, poor man's Markdown processing to at least respect line
     // breaks
     text = text.replace(/\n/g, '<br>\n');
 
-    var recognizableDict = {};
-    var recognizableArr = [];
-    // var uniqueKanji = _.unique(text.match(han));
-    var html = XRegExp.replace(text, han, function(fullMatch, kanji, idx, str) {
-        if (kanji in app['known-kanji']) {
-            if (!(kanji in recognizableDict)) {
-                recognizableDict[kanji] = idx;
-                recognizableArr.push({kanji : kanji, firstAppearance : idx});
-            }
+    // Use app['recognizable-kanji'] as a dictionary for fast lookups, then
+    // convert it to an array at the end. Clear this and regenerate it. We have
+    // no use for kanji that were previously recognizeable (only kanji that were
+    // previously recognized!).
+    app['recognizable-kanji'] = {};
 
-            return '<span class="any-kanji known-kanji ' + kanji + '">' +
+    // Find intersection of kanji that the user knows and kanji in the text.
+    // Build the HTML representation of text: put all kanji in span tags with
+    // classes that indicate their kanji-ness and whether or not they should be
+    // known to the user.
+    var html = XRegExp.replace(text, han, function(fullMatch, kanji, idx, str) {
+        // If it's a kanji the user will recognize, tag it with
+        // .recognizable-kanji and .known-kanji. Also insert it into
+        // app['recognizable-kanji'] as a key and value being the earliest index
+        // it appears. We'll convert it to a sorted array after we're done
+        // building HTML.
+        if (kanji in app['known-kanji']) {
+            if (!(kanji in app['recognizable-kanji'])) {
+                app['recognizable-kanji'][kanji] = idx;
+            }
+            return '<span class="any-kanji recognizable-kanji ' + kanji + '">' +
                    kanji + '</span>';
         }
+        // Otherwise, just tag it with .any-kanji
         return '<span class="any-kanji ' + kanji + '">' + kanji + '</span>';
     }, 'all');
-    d3.select("#redisplay").html(html);
-    app['recognizable-kanji'] = recognizableArr;
-    // app['recognized-kanji'] = {};
+    // Convert the dictionary to an array, sorted by index of first appearance
+    // in input text.
+    app['recognizable-kanji'] = objectToKeysSortedArray(app['recognizable-kanji']);
 
-    d3.selectAll('.known-kanji').on('click', function() {
+    // Calculate some extra data for stats.
+    var unknownKanji =
+        _.difference(_.unique(text.match(han)), app['recognizable-kanji']);
+    app['unknown-kanji'] = unknownKanji;
+
+    // Render the resulting HTML
+    d3.select("#redisplay").html(html);
+
+    // For all the recognizable-kanji, classed as such, add a click-listener
+    // that'll add it to app['recognized-kanji'] and then run updateRecognized()
+    // which processes app['recognized-kanji'].
+    d3.selectAll('.recognizable-kanji').on('click', function() {
         var thisKanji = this.innerText;
         if (!(thisKanji in app['recognized-kanji'])) {
             app['recognized-kanji'][thisKanji] = 1;
@@ -71,71 +128,141 @@ var japaneseInputChanged = function() {
         updateRecognized();
     });
 
-    showAllRecognizable();
+    // Note that this click-listener will only add keys to the app
+    // ['recognized-kanji'] dictionary object. There may be kanji in there
+    // that are no longer in app['recognizable-kanji'], i.e., the user might
+    // have removed that kanji from the input or from the set of known kanji.
+    // When there are kanji that the user has clicked as 'recognized' before
+    // but that are no longer going to appear in the text or that they no
+    // longer wish to treat as known, these will be indicated differently.
+    // Therefore, run updateRecognized() here to make sure all kanji already
+    // recognized are fresh with respect to the changes in this function.
     updateRecognized();
+
+    // Recall that this function runs whenever the input text changes or when
+    // the set of known kanji changes, so in both of those cases, we need to
+    // update the answer key.
+    buildAnswerKey();
 };
 
-// need some way of deleting recognized kanji via DOM from DOM &
-// app['recognized-kanji']
+// This function gets run whenever app['recognized-kanji'] or
+// app['recognizable-kanji'] changes. Note two things in the previous sentence:
+// "or" and "changes".
+//
+// This function is run when *either* of those dictionaries changes.
+// app['recognized-kanji'] has kanji for keys, which the user has previously
+// indicated as recognized; app['recognizable-kanji'] is an array containing
+// kanji that appear in the input text that the user ought to recognize. The
+// latter can change when the user changes the input text, or even the set of
+// known kanji (changing the Heisig number, etc.). Kanji that are in
+// app['recognized-kanji'] but not in app['recognizable-kanji'] will be given
+// the DOM class "recognized-kanji-not-currently-recognizable" (maybe colored
+// yellow).
+//
+// This function is run when either of those dictionaries *changes*. Kanji keys
+// may be added or removed quite freely without becoming inconsistent. This
+// function will ensure that the DOM display reflects the unstructured ways that
+// these two dictionaries may change.
 function updateRecognized() {
-    // turn all off then selectively turn some on ***in the redisplay***
+    // This function does a lot of work, in that it reduces everything to some
+    // known base state and then treats anything that needs special treatment.
+    // So it does some wasteful work but I think that leads to understandeable
+    // code. If performance ever becomes an issue, it can be addressed then.
+
+
+    var recognizedInText = _.intersection(_.keys(app['recognized-kanji']), app['recognizable-kanji']);
+
+    var stats = d3.select("#stats");
+    var numUnknown = app['unknown-kanji'].length;
+    var numRecognizable = app['recognizable-kanji'].length;
+    var numTotalKanji = numUnknown + numRecognizable;
+    var numLeftToRecognize = numRecognizable - recognizedInText.length;
+    stats.html("");
+    var list = stats.append("ul");
+    var appendToList = function(s) { list.append("li").text(s); };
+    appendToList("Number of kanji you ought to know in input text: " +
+                 numRecognizable +
+                 ((numLeftToRecognize == 0)
+                      ? " (all found!)"
+                      : (" (" + (numLeftToRecognize == numRecognizable
+                                     ? "all"
+                                     : numLeftToRecognize) +
+                         " remain to be found)")));
+    appendToList("Total number of kanji in input text, known and unknown: " + numTotalKanji);
+    appendToList("Total number of kanji you know: " + _.keys(app['known-kanji']).length);
+    if (numUnknown > 0) {
+        appendToList("Number of kanji you need to learn to read input text: " + numUnknown);
+    }
+
+    // In the redisplay (the thing that you click on to select kanji), remove
+    // the ".recognized-kanji" class from all kanji, then add it back to ones
+    // that have been previously recognized. (Some previously recognized kanji
+    // may not be available in the current input text, it doesn't matter, those
+    // D3 CSS selections will just be empty.)
     d3.selectAll('.any-kanji').classed('recognized-kanji', false);
-    _.intersection(_.keys(app['recognized-kanji']),
-                   _.pluck(app['recognizable-kanji'], 'kanji'))
-        .forEach(function(aKanji) {
+    recognizedInText.forEach(function(aKanji) {
         d3.selectAll('.any-kanji.' + aKanji).classed('recognized-kanji', true);
     });
 
-    // In *recognition* section, update the recognized-but-not-in-text blocks
-    // manually. This is because data.enter()/.exit() won't do anything when we
-    // have all the kanji already in app['recognized-kanji'] and go from Heisig
-    // number 1 to 2200.
-     _.difference(_.keys(app['recognized-kanji']),
-                 _.pluck(app['recognizable-kanji'], 'kanji'))
-        .forEach(function(aKanji) {
-        d3.selectAll('#recognition .' + aKanji).classed({
-            'recognized-kanji' : false,
-            'recognized-kanji-not-in-text' : true
-        });
-    });
-    _.intersection(_.keys(app['recognized-kanji']),
-                 _.pluck(app['recognizable-kanji'], 'kanji'))
-        .forEach(function(aKanji) {
-        d3.selectAll('#recognition .' + aKanji).classed({
-            'recognized-kanji' : true,
-            'recognized-kanji-not-in-text' : false
-        });
-    });
-
-    // Update the keyword input area
+    // All the D3 data-binding magic happens here, in the recognition section,
+    // which displays the kanji you've recognized with a box for keywords. The
+    // data array is the kanji that are both recognizable *and* previously-
+    // recognized (set intersection). Kanji that are no longer recognizable
+    // (not in input text, or in the set of known kanji) are still there in the
+    // DOM, and will have to be classed with
+    // '.recognized-kanji-not-currently-recognizable'; we access these elements
+    // via D3's exit() method. Then kanji that are newly recognized, which don't
+    // exit in the DOM, are accessed via enter(), and are added to the DOM.
+    //
+    // But first, find the existing div DOM elements and bind the new array to
+    // them. The kanji is used as the data key.
     var data = d3.select('#recognition').selectAll('div').data(
-        _.intersection(_.keys(app['recognized-kanji']),
-                       _.pluck(app['recognizable-kanji'], 'kanji')),
-        function(d) { return d; });
+        recognizedInText, function(d) { return d; });
 
-    var ps = data.enter().append("div").classed('recognized-container', true);
-    ps.append("span")
+    // For ALL the DOM elements bound to data (even ones that are missing from
+    // the current data array), add the '.recognized-kanji' class, and then
+    // for the DOM elements that don't match any current data element (via
+    // D3's exit()), remove that class.
+    data.classed({
+        'recognized-kanji' : true,
+        'recognized-kanji-not-currently-recognizable' : false
+    });
+    data.exit().classed({
+        'recognized-kanji' : false,
+        'recognized-kanji-not-currently-recognizable' : true
+    });
+    // Now, kanji with class '.recognized-kanji-not-currently-recognizable' can
+    // be colored yellow or something like that.
+
+    // For data elements that don't yet have a DOM entry, create them. They will
+    // be inside <div> tags, with a <span> tag containing the kanji, an <input>
+    // tag for the keyword, and another <span> tag containing a button to delete
+    // this kanji from app['recognized-kanji'].
+    var newDivs = data.enter().append("div").classed({
+        'recognized-container' : true,
+        'recognized-kanji' : true,
+        'recognized-kanji-not-currently-recognizable' : false
+    });
+
+    newDivs.append("span")
         .property({
             "id" : function(d) { return "recognized-" + d; },
         })
         .attr('class', function(d) { return d; })
-        .classed({
-            'recognized-kanji' : true,
-            'recognized-kanji-not-in-text' : false
-        })
-        .text(function(d) { return d + " "; });
+        .classed({'kanji-for-recognition': true})
+        .text(function(d) { return d; });
 
-    ps.append("input")
+    newDivs.append("form")
+        .classed('pure-form', true)
+        .append("input")
         .property({
             type : "text",
             size : "10",
-                   // this isn't the answer key!
-                   // value : function(d) { return kw[kanji2number[d]]; },
         })
         .on("input", function(d) {
-            var thisKw = this.value;
+            var thisKw = this.value.toLowerCase();
             _foo = this;
-            if (thisKw === kw[kanji2number[d]]) {
+            if (thisKw === kw[kanji2number[d]].toLowerCase()) {
                 // Don't bother telling EVERYONE you've now correctly added
                 // keyword:
                 // d3.selectAll('.recognized-kanji.' +
@@ -147,22 +274,27 @@ function updateRecognized() {
                 this.classList.remove('recognized-keyword');
             }
         });
-    ps.append('span').text('×').classed('like-link', true).on('click',
-                                                              function(d) {
+
+    newDivs.append('span').text('×').classed('like-link', true).on('click',
+                                                                   function(d) {
         this.parentNode.remove();
         delete app['recognized-kanji'][d];
         updateRecognized();
     });
 }
 
-function showAllRecognizable() {
+function objectToKeysSortedArray(obj) {
+    return _.sortBy(_.keys(obj), function(key) { return obj[key]; });
+}
+
+function buildAnswerKey() {
     var data = d3.select("#answers").selectAll("p").data(
-        app['recognizable-kanji'], function(d) { return d.kanji; });
+        app['recognizable-kanji'], function(d) { return d; });
 
     data.exit().remove();
 
     var ps = data.enter().append("p").text(
-        function(d) { return d.kanji + " " + kw[kanji2number[d.kanji]]; });
+        function(d) { return d + " " + kw[kanji2number[d]]; });
 }
 
 $(document).ready(function() {
@@ -174,13 +306,20 @@ $(document).ready(function() {
     // Helper function to attach a listener to an input field when an
     // appropriate radio box is enabled
     function inputAndRadioAddListener(inputSelector, radioSelector, func) {
-        d3.select(inputSelector).on('input', function() {
+        var innerFunc = function() {
             if (d3.select(radioSelector).property('checked')) {
                 app['known-kanji'] =
                     func(d3.select(inputSelector).property('value'));
                 japaneseInputChanged();
             }
+        };
+        d3.select(inputSelector).on('click', function() {
+            d3.select(radioSelector).property('checked', true);
+            innerFunc();
         });
+        d3.select(inputSelector).on('input', innerFunc);
+
+        d3.select(radioSelector).on('click', innerFunc);
     }
     inputAndRadioAddListener("#heisig-number", "#use-heisig-number",
                              function(x) {
@@ -199,19 +338,11 @@ $(document).ready(function() {
     // Run once
     japaneseInputChanged();
 
-    // If you click on the custom kanji box, select its radio button
-    d3.select("#custom-kanji-list").on('click', function() {
-        d3.select("#use-kanji-list").property('checked', true);
-    });
-    // And similarly for the Heisig number box
-    d3.select("#heisig-number").on('click', function() {
-        d3.select("#use-heisig-number").property('checked', true);
-    });
-
     d3.select('button#recognize-kanji-button').on('click', function() {
-        app['recognized-kanji'] =
-            _.invert(_.union(_.keys(app['recognized-kanji']),
-                             _.pluck(app['recognizable-kanji'], 'kanji')));
+        // This turns out to not change the sort order of kanjiSortedArray,
+        // which is great!
+        app['recognized-kanji'] = _.invert(
+            _.union(_.keys(app['recognized-kanji']), app['recognizable-kanji']));
         updateRecognized();
     });
 
